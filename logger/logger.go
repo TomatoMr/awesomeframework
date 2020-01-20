132c71b70b150ba3510f828fb0d54242cf4ed56c
v_vhclin <v_vhclin@tencent.com>
2020-01-20 11:11:56 +0800

    feat(logger): init logger.

--- logger/logger.go
+++ logger/logger.go
@@ -1,1 +1,48 @@
 package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
+)
+
+var l *zap.Logger
+
+func InitLogger(logPath, logLevel string) error {
+	hook := lumberjack.Logger{
+		Filename:   logPath,
+		MaxSize:    1024,
+		MaxBackups: 3,
+		MaxAge:     7,
+		Compress:   true,
+	}
+	w := zapcore.AddSync(&hook)
+
+	var level zapcore.Level
+	switch logLevel {
+	case "debug":
+		level = zap.DebugLevel
+	case "info":
+		level = zap.InfoLevel
+	case "error":
+		level = zap.ErrorLevel
+	default:
+		level = zap.DebugLevel
+	}
+
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	core := zapcore.NewCore(
+		zapcore.NewConsoleEncoder(encoderConfig),
+		w,
+		level,
+	)
+
+	l = zap.New(core)
+
+	return nil
+}
+
+func GetLogger() *zap.Logger {
+	return l
+}
