39ee1630430488b727adf8879dbd8e4e5e5dc748
TomatoMr <v_vhclin@tencent.com>
2020-01-21 16:06:45 +0800

    feat(controller): add controller

--- process/controller/controller.go
+++ process/controller/controller.go
@@ -1,1 +1,19 @@
 package controller
+
+import (
+	"net/http"
+	"time"
+)
+
+type ServerTimeReq struct {
+}
+
+type ServerTimeResp struct {
+	ServerTime int64 `json:"server_time"`
+}
+
+func GetServerTime() (ServerTimeResp, int, string) {
+	resp := ServerTimeResp{}
+	resp.ServerTime = time.Now().Unix()
+	return resp, http.StatusOK, ""
+}
