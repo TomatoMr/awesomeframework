c6f0010bac93249df0cd0b1b29234322b55e1be2
v_vhclin <v_vhclin@tencent.com>
2020-02-15 10:02:48 +0800

    feat(ws): add websocket and test

--- process/http/http.go
+++ process/http/http.go
@@ -2,21 +2,54 @@ package http
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 	"go.uber.org/zap"
+	"net/http"
 	"os"
 )
 
 var engine *gin.Engine
+var upGrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
 
 func StartHttpServer(addr string) {
 	engine = gin.Default()
 	Route()
+
 	if err := engine.Run(addr); err != nil {
 		zap.Error(err)
 		os.Exit(1)
 	}
 }
 
+//webSocket请求ping 返回pong
+func Ws(ctx *gin.Context) {
+	ws, err := upGrader.Upgrade(ctx.Writer, ctx.Request, nil)
+	if err != nil {
+		return
+	}
+	defer ws.Close()
+	for {
+		//读取数据
+		mt, message, err := ws.ReadMessage()
+		if err != nil {
+			break
+		}
+		if string(message) == "ping" {
+			message = []byte("pong")
+		}
+		//写入数据
+		err = ws.WriteMessage(mt, message)
+		if err != nil {
+			break
+		}
+	}
+}
+
 func Route() {
 	engine.GET("/server_time", GetServerTime)
+	engine.GET("/ws", Ws)
 }
---
+++ process/http/http_test.go
@@ -0,0 +1,38 @@
+package http
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+func TestWs(t *testing.T) {
+	var dialer *websocket.Dialer
+
+	conn, _, err := dialer.Dial("ws://127.0.0.1:8080/ws", nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	go timeWriter(conn)
+
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println("read:", err)
+			return
+		}
+
+		fmt.Printf("received: %s\n", message)
+	}
+}
+
+func timeWriter(conn *websocket.Conn) {
+	for {
+		time.Sleep(time.Second * 2)
+		conn.WriteMessage(websocket.TextMessage, []byte(time.Now().Format("2006-01-02 15:04:05")))
+	}
+}
