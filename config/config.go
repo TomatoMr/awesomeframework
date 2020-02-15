87f761584adccc42240fc108c96f4a43dbe49eb5
v_vhclin <v_vhclin@tencent.com>
2020-02-15 10:01:56 +0800

    fix(config): change json to yaml

--- config/config.go
+++ config/config.go
@@ -1,44 +1,44 @@
 package config
 
 import (
-	"encoding/json"
 	"github.com/pkg/errors"
+	"gopkg.in/yaml.v2"
 	"io/ioutil"
 )
 
 type LogConfig struct {
-	LogPath  string `json:"log_path"`
-	LogLevel string `json:"log_level"`
+	LogPath  string `yaml:"log_path"`
+	LogLevel string `yaml:"log_level"`
 }
 
 type DBConfig struct {
-	DbHost     string `json:"db_host"`
-	DbPort     string `json:"db_port"`
-	DbUser     string `json:"db_user"`
-	DbPassword string `json:"db_password"`
-	DbName     string `json:"db_name"`
+	DbHost     string `yaml:"db_host"`
+	DbPort     string `yaml:"db_port"`
+	DbUser     string `yaml:"db_user"`
+	DbPassword string `yaml:"db_password"`
+	DbName     string `yaml:"db_name"`
 }
 
 type RedisConfig struct {
-	Addr     string `json:"addr"`
-	Password string `json:"password"`
-	DB       int    `json:"db"`
+	Addr     string `yaml:"addr"`
+	Password string `yaml:"password"`
+	DB       int    `yaml:"db"`
 }
 
 type HttpConfig struct {
-	Addr string `json:"addr"`
+	Addr string `yaml:"addr"`
 }
 
 type RpcConfig struct {
-	Addr string `json:"addr"`
+	Addr string `yaml:"addr"`
 }
 
 type Config struct {
-	LogConfig   LogConfig   `json:"log_config"`
-	DBConfig    DBConfig    `json:"db_config"`
-	RedisConfig RedisConfig `json:"redis_config"`
-	HttpConfig  HttpConfig  `json:"http_config"`
-	RpcConfig   RedisConfig `json:"rpc_config"`
+	LogConfig   LogConfig   `yaml:"log_config"`
+	DBConfig    DBConfig    `yaml:"db_config"`
+	RedisConfig RedisConfig `yaml:"redis_config"`
+	HttpConfig  HttpConfig  `yaml:"http_config"`
+	RpcConfig   RedisConfig `yaml:"rpc_config"`
 }
 
 var conf Config
@@ -46,13 +46,11 @@ var conf Config
 func InitConfig(configPath string) error {
 	configFile, err := ioutil.ReadFile(configPath)
 	if err != nil {
-		err = errors.Wrap(err, "Read config file failed.")
-		return err
+		return errors.Wrap(err, "Read config file failed")
 	}
-	err = json.Unmarshal(configFile, &conf)
-	if err != nil {
-		err = errors.Wrap(err, "Unmarshal config file failed.")
-		return err
+
+	if err = yaml.Unmarshal(configFile, &conf); err != nil {
+		return errors.Wrap(err, "Unmarshal config file failed.")
 	}
 	return nil
 }
